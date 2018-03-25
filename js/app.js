4463b98ef704297e5d8bdf6800b4c0c2124b5df0
Rui Rodrigues <rman2006@hotmail.com>
2018-03-25 23:31:24 +0100

    Added flex
    delivery 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-﻿// @ts-check
+﻿
+var totalalbums = 0; listedalbums = 0;
 
-var cds;
+
 // Registar o nosso "main" para quando o DOM está carregado.
 // O 'DOMContentLoaded' é executado ligeiramente antes do 'load',
 // logo, quando possível, deve ser usado.
@@ -10,101 +11,52 @@ document.addEventListener('DOMContentLoaded', function main(e) {
     xhr.onload = function xmlLoaded(e) {  // Executado quando o conteúdo é carregado.
         if (xhr.status === 200) {  // OK
             var xml = xhr.responseXML;
-
-
             // 'xml' é um documento, tal como o 'document'.
             // Podemos desempenhar as mesmas tarefas nele que no 'document'.
 
+            var appContainer = document.createElement('div');
+            appContainer.setAttribute('id', 'app-container');
+
+            
+            var bcont = document.createElement('div');
+            bcont.setAttribute('id', 'discoteca');
+            var topbar = document.createElement('div');
+            topbar.setAttribute('class', 'top-bar');
+            var stats = document.createElement('div');
+            topbar.appendChild(stats);
 
             //search box for albuns
             var searchAlbum = document.createElement('input');
-            searchAlbum.setAttribute('type', 'text');
-            searchAlbum.setAttribute('id', 'searchAlbum');
-            searchAlbum.setAttribute('name', 'searchAlbum');
-            searchAlbum.addEventListener('input', function onChangeAlbum(evt) {
-                //                evt.preventDefault();
-                var filter = searchAlbum.value.toUpperCase();
-                var albuns = document.querySelectorAll('div');
-                for (var i = 0, l = albuns.length; i < l; i++) {
-                    if (albuns[i].querySelector('h1').textContent.toUpperCase().includes(filter)) {
-                        albuns[i].style.display = "";
-                    } else {
-                        albuns[i].style.display = "none";
-                    }
-                }
+            searchAlbum.setAttribute('type', 'text'); 
+            searchAlbum.setAttribute('value', 'search by album name');
+            searchAlbum.setAttribute('size', '30');
+            searchAlbum.setAttribute('class', 'searchBox'); 
+            searchAlbum.addEventListener('focus', function onFocusAlbum(evt) {
+                searchAlbum.setAttribute('value', '');
             });
-            document.body.appendChild(searchAlbum);
+            searchAlbum.addEventListener('blur', function onBlurAlbum(evt) {
+                searchAlbum.setAttribute('value', 'search by album name');
+            });
+            searchAlbum.addEventListener('input', function onChangeAlbum(evt) {
+                var filter = searchAlbum.value, cds;
+                var cds = filter != "" ? xml.querySelectorAll(`cd[titulo*="${filter}" i]`) : xml.querySelectorAll(`cd`);
+                stats.innerHTML = "";
+                stats.insertAdjacentHTML("afterbegin", `<p>${cds.length} of ${totalalbums}</p>`);
+                bcont.innerHTML = "";
+                if (cds.length != 0) listaCds(cds, bcont); else bcont.innerHTML = "<p><i>no results found</i></p>";
+            });
+            topbar.appendChild(searchAlbum);
+            appContainer.appendChild(topbar);
+            appContainer.appendChild(bcont);
+            document.body.appendChild(appContainer);
+
 
             // Colocar os CDs do XML na página.
-            cds = xml.querySelectorAll('cd');
-            for (var i = 0, ncds = cds.length; i < ncds; i++) {
-                //xml.querySelector('cd:nth-of-type(i) > capa').getAttribute('imagMini');
-                var cd = cds[i];
+            var cds = xml.querySelectorAll('cd');
+            totalalbums = cds.length;
+            stats.insertAdjacentHTML("afterbegin", `<p>${totalalbums} of ${totalalbums}</p>`);
+            listaCds(cds, bcont);
 
-                let container = document.createElement('div');
-                container.setAttribute('id', `div${i}`);
-                container.setAttribute('name', `div${i}`);
-                container.insertAdjacentHTML("afterbegin", "<hr>");
-
-                var AlbumTitle = document.createElement('H1');
-                putT(cd.getAttribute('titulo'), container, AlbumTitle);
-
-                var AlbumAuthor = document.createElement('H2');
-                putT("by " + cd.getAttribute('autoria'), container, AlbumAuthor);
-
-                var AlbumBuy = document.createElement('a');
-                AlbumBuy.setAttribute('href', `http://${cd.getAttribute('amazon')}`);
-                AlbumBuy.textContent = "Buy on Amazon";
-                container.appendChild(AlbumBuy);
-
-                var AlbumEdit = document.createElement('p');
-                putT(cd.getAttribute('editora') + " (" + getData(cd.querySelector('data')) + ")", container, AlbumEdit);
-
-                var AlbumCover = document.createElement('img');
-                AlbumCover.setAttribute('src', `assets/images/${cd.querySelector('capa').getAttribute('imagMini')}`);
-                container.appendChild(AlbumCover);
-
-                AlbumCover.insertAdjacentHTML("afterend", "<br>");
-
-                //search box for tracks
-                let searchTracks = document.createElement('input');
-                searchTracks.setAttribute('type', 'text');
-                searchTracks.setAttribute('id', `searchTracks${i}`);
-                searchTracks.setAttribute('name', `searchTracks${i}`);
-                searchTracks.addEventListener('input', function onChangeTracks(evt) {
-                    // evt.preventDefault();
-                    // evt.stopPropagation();
-                    var filter = searchTracks.value.toUpperCase();
-                    let tracks = container.querySelectorAll('li');
-                    for (var j = 0, l = tracks.length; j < l; j++) {
-                        if (tracks[j].textContent.toUpperCase().includes(filter)) {
-                        //if (a.indexOf(filter) > -1) {
-                            tracks[j].style.display = "";
-                        } else {
-                            tracks[j].style.display = "none";
-                        }
-                    }
-                });
-                container.appendChild(searchTracks);
-
-
-                var conteudos = cd.querySelectorAll("conteudo");
-                for (var j = 0, n = conteudos.length; j < n; j++) {
-                    if (n > 1) {
-                        var DiskNum = document.createElement('H3');
-                        putT(`Disk ${j + 1}`, container, DiskNum);
-                    }
-                    var olist = document.createElement('ol');
-                    var faixas = conteudos[j].querySelectorAll('faixa');
-                    for (var k = 0, o = faixas.length; k < o; k++) {
-                        var faixa = faixas[k];
-                        var ilist = document.createElement('li');
-                        putT(faixa.getAttribute('ref'), olist, ilist);
-                    }
-                    container.appendChild(olist);
-                }
-                document.body.appendChild(container);
-            }
 
         } else {  // Erro
             console.error(`Status ${xhr.status}`, xhr.responseText);
@@ -114,7 +66,6 @@ document.addEventListener('DOMContentLoaded', function main(e) {
     xhr.onerror = function communicationsError(e) {  // Problema de comunicação.
         console.error('An error occured.', e);
     };
-
     xhr.send();  // Enviar o pedido.
 });
 
@@ -128,4 +79,86 @@ function getData(from) {
     var mes = from.getAttribute('mes');
     var ano = from.getAttribute('ano');
     return `${dia}/${mes}/${ano}`
+}
+
+function listaCds(cds, cont) {
+
+    for (var i = 0, ncds = cds.length; i < ncds; i++) {
+        //xml.querySelector('cd:nth-of-type(i) > capa').getAttribute('imagMini');
+        let cd = cds[i];
+
+        let container = document.createElement('div');
+        container.setAttribute('id', `div${i}`);
+        container.setAttribute('name', `div${i}`);
+      
+
+        var topinfo = document.createElement("div")
+        let tracksinfo = document.createElement("div")
+        var AlbumCover = document.createElement('img');
+        AlbumCover.setAttribute('src', `assets/images/${cd.querySelector('capa').getAttribute('imagMini')}`);
+        topinfo.appendChild(AlbumCover);
+
+        var AlbumTitle = document.createElement('H1');
+        putT(cd.getAttribute('titulo'), topinfo, AlbumTitle);
+
+        var AlbumAuthor = document.createElement('H2');
+        putT("by " + cd.getAttribute('autoria'), topinfo, AlbumAuthor);
+       // putT(cd.getAttribute('autoria'), sidebar, AlbumAuthor);
+
+        var AlbumBuy = document.createElement('a');
+        AlbumBuy.setAttribute('href', `http://${cd.getAttribute('amazon')}`);
+        AlbumBuy.textContent = "Buy on Amazon";
+        topinfo.appendChild(AlbumBuy);
+
+        var AlbumEdit = document.createElement('p');
+        putT(cd.getAttribute('editora') + " (" + getData(cd.querySelector('data')) + ")", topinfo, AlbumEdit);
+
+ 
+        AlbumCover.insertAdjacentHTML("afterend", "<br>");
+
+        //search box for tracks
+        let olist;
+        let searchTracks = document.createElement('input');
+        searchTracks.setAttribute('class', 'searchBox'); 
+        searchTracks.setAttribute('type', 'text');
+        searchTracks.addEventListener('input', function onChangeTracks(evt) {
+            var filter = searchTracks.value;
+            var olists = tracksinfo.querySelectorAll("ol");
+            for (var i = 0, n = olists.length; i < n; i++) {
+                while (olists[i].firstChild) {
+                    olists[i].removeChild(olists[i].firstChild);
+                }
+            }
+            for (var i = 0, n = olists.length; i < n; i++) {
+                var faixas = filter != "" ? cd.querySelectorAll("conteudo")[i].querySelectorAll(`faixa[ref*="${filter}" i]`) : cd.querySelectorAll("conteudo")[i].querySelectorAll(`faixa`);
+
+                console.log(faixas);
+                if (faixas.length != 0) listaTracks(faixas, olists[i]); else olists[i].innerHTML = "<p><i>no results found</i></p>";
+            }
+        });
+        tracksinfo.appendChild(searchTracks)
+        //coloca a lista de faixas na pagina
+        let conteudos = cd.querySelectorAll("conteudo");
+        for (var j = 0, n = conteudos.length; j < n; j++) {
+            if (n > 1) {
+                var DiskNum = document.createElement('H3');
+                putT(`Disk ${j + 1}`, tracksinfo, DiskNum);
+            }
+            olist = document.createElement('ol');
+            let faixas = conteudos[j].querySelectorAll('faixa');
+            listaTracks(faixas, olist);
+            tracksinfo.appendChild(olist);
+        }
+        container.appendChild(topinfo);
+        container.appendChild(tracksinfo);
+        cont.appendChild(container);
+    }
+}
+
+function listaTracks(faixas, olist) {
+    for (var k = 0, o = faixas.length; k < o; k++) {
+        var faixa = faixas[k];
+        var ilist = document.createElement('li');
+        putT(faixa.getAttribute('ref'), olist, ilist);
+    }
 }
